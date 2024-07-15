acd4544e2f67fb7900934e2bab3b848570c6991c
patak <patak.adi@gmail.com>
2024-07-15 11:06:12 +0000

    16 july 2024

---
+++ src/ex_16072024/Lab003.java
@@ -0,0 +1,11 @@
+package ex_16072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 30.5;
+        double c = a + b;
+        System.out.println(c);
+    }
+
+}

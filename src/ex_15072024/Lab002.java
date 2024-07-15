7e3acf57b021bf6d35a47a66f1a7e680f81f9d75
patak <patak.adi@gmail.com>
2024-07-15 09:51:52 +0000

    15 july 2024

---
+++ src/ex_15072024/Lab002.java
@@ -0,0 +1,11 @@
+package ex_15072024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int b = 10;
+        int a = 10;
+        int c = (a + b);
+        System.out.println("c");
+
+    }
+}
